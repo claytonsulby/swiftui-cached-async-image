427fbfa002a6d92f493eefc60fc9c99b516540ca
claytonsulby <claytonsulby@gmail.com>
2024-06-14 07:30:30 -0400

    changes

--- Sources/CachedAsyncImage/CachedAsyncImage.swift
+++ Sources/CachedAsyncImage/CachedAsyncImage.swift
@@ -304,7 +304,8 @@ public struct CachedAsyncImage<Content>: View where Content: View {
         
         self._phase = State(wrappedValue: .empty)
         do {
-            if let urlRequest = urlRequest, let image = try cachedImage(from: urlRequest, cache: urlCache) {
+            if let urlRequest = urlRequest,
+               let image = try cachedImage(from: urlRequest, cache: urlCache) {
                 self._phase = State(wrappedValue: .success(image))
             }
         } catch {
@@ -316,7 +317,9 @@ public struct CachedAsyncImage<Content>: View where Content: View {
     private func load() async {
         do {
             if let urlRequest = urlRequest {
+                
                 let (image, metrics) = try await remoteImage(from: urlRequest, session: urlSession)
+                
                 if metrics.transactionMetrics.last?.resourceFetchType == .localCache {
                     // WARNING: This does not behave well when the url is changed with another
                     phase = .success(image)
@@ -352,18 +355,25 @@ private extension AsyncImage {
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 private extension CachedAsyncImage {
     private func remoteImage(from request: URLRequest, session: URLSession) async throws -> (Image, URLSessionTaskMetrics) {
+        
         let (data, _, metrics) = try await session.data(for: request)
+        
         if metrics.redirectCount > 0, let lastResponse = metrics.transactionMetrics.last?.response {
+            
             let requests = metrics.transactionMetrics.map { $0.request }
             requests.forEach(session.configuration.urlCache!.removeCachedResponse)
+            
             let lastCachedResponse = CachedURLResponse(response: lastResponse, data: data)
             session.configuration.urlCache!.storeCachedResponse(lastCachedResponse, for: request)
+            
         }
         return (try image(from: data), metrics)
     }
     
     private func cachedImage(from request: URLRequest, cache: URLCache) throws -> Image? {
-        guard let cachedResponse = cache.cachedResponse(for: request) else { return nil }
+        guard let cachedResponse = cache.cachedResponse(for: request) else {
+            return nil
+        }
         return try image(from: cachedResponse.data)
     }
     
@@ -402,5 +412,28 @@ private extension URLSession {
         let controller = URLSessionTaskController()
         let (data, response) = try await data(for: request, delegate: controller)
         return (data, response, controller.metrics!)
+    }
+}
+
+
+@available(iOS 15.0, macOS 12.0, *)
+struct CachedAsyncImage_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            List {
+                CachedAsyncImage(url: URL(string: "http://via.placeholder.com/160x90&text=hello"))
+                    .previewLayout(.sizeThatFits)
+                
+                CachedAsyncImage(url: URL(string: "http://via.placeholder.com/160x90&text=hello"), content: { image in
+                    image
+                        .resizable()
+                }, placeholder: {
+                    ProgressView()
+                })
+                .previewLayout(.sizeThatFits)
+            }
+        }
+
+        
     }
 }
